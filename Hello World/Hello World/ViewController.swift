c3a4e1ef588cff6db10ec0f1fb905bb2967c3a5c
Edgar Jair <edgar.gs90@gmail.com>
2015-05-30 01:30:18 -0500

    second Commit

--- Hello World/Hello World/ViewController.swift
+++ Hello World/Hello World/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet var lblOne: UILabel!
+    
+    override func viewDidLoad()
+    {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        lblOne.text = "Hello World"
     }
 
     override func didReceiveMemoryWarning() {
