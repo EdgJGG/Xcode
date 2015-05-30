d1bfc0f6a94fc281247004bbb78a50e2ac1aa4ac
Edgar Jair <edgar.gs90@gmail.com>
2015-05-30 02:00:53 -0500

    second commit added label

--- Github Test 1/Github Test 1/ViewController.swift
+++ Github Test 1/Github Test 1/ViewController.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
+    @IBOutlet var lblOne: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        lblOne.text = "First Label"
     }
 
     override func didReceiveMemoryWarning() {
